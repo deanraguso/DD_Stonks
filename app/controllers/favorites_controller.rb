0d6f436fc3fc7379c4e8977ed8df21b1bf200ead
dean <deanraguso@gmail.com>
2021-05-11 11:26:31 +1000

    Created: Favorites actions and appropriate routing, each requiring an :id param

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,9 +1,24 @@
 class FavoritesController < ApplicationController
+
+    def show
+
+    end
+
     def add
-        
+
     end
 
     def delete
 
+    end
+
+    def erase
+
+    end
+
+    private 
+
+    def user
+        user = User.find_by(id: params.require(:id))
     end
 end
