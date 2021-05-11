987ab3e6b676eaf21751330c2cdbf456dc8d40b9
dean <deanraguso@gmail.com>
2021-05-11 10:38:19 +1000

    Created: Pages controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root "pages#index"
+  get 'pages/show', as: '/show'
+  get 'pages/about', as: '/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
