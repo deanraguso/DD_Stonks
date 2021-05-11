88cbbb99e9a3452d117bd1445bcfc25ac4f3ce5d
dean <deanraguso@gmail.com>
2021-05-11 21:16:53 +1000

    Altered: Configured lib to be auto-loaded on startup, to keep the contants module in RAM

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.autoload_paths += Dir["#{config.root}/lib/**/"]
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
